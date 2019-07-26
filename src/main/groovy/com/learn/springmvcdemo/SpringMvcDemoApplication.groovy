6ee3dca92f58233ac39a94d9bcf1eef17c1a5244
Kishor Ganeshrao <kishorkg@yahoo.com>
2019-07-26 22:15:09 +1200

    initial commit

---
+++ src/main/groovy/com/learn/springmvcdemo/SpringMvcDemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.learn.springmvcdemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringMvcDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringMvcDemoApplication, args)
+	}
+
+}
